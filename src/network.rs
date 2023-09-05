40fcce52a0d0fb6f8db8f9d21e5267cb5208efd1
Dylan Laufenberg <edev455@gmail.com>
2023-09-05 06:42:01 -0700

    Implement Display for Report.

--- src/network.rs
+++ src/network.rs
@@ -19,6 +19,7 @@ use crate::core::action::Action;
 use crate::executor;
 #[cfg(doc)]
 use crate::executor::Executor;
+use std::fmt;
 use std::sync::Arc;
 
 /// The UI's channels of communication with the rest of Sira (through [Executor]).
@@ -68,6 +69,7 @@ pub enum Report {
     /// This message does not imply success; for the outcome, see the [result] field.
     ///
     /// [result]: Result::ActionResult::result]
+    // TODO Rename ActionResult, e.g. to ActionReport, to avoid confusion with Result types.
     ActionResult {
         host: String,
         // Add `plan: String,` here if we give plans names, which we should if we let them queue.
@@ -78,4 +80,224 @@ pub enum Report {
         action: Arc<Action>,
         result: Result<(), ()>,
     },
+}
+
+impl fmt::Display for Report {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Report::*;
+        match self {
+            // The network module is about to try connecting to the specified host.
+            Connecting(host) => write!(f, "Connecting to {}", host),
+
+            // The network module has successfully connected to the specified host.
+            Connected(host) => write!(f, "Connected to {}", host),
+
+            // The network module couldn't reach the host.
+            FailedToConnect { host, error } => write!(f, "Couldn't connect to {}: {}", host, error),
+
+            // The network module had a connection to the host, but the connection is now closed.
+            Disconnected { host, error: None } => write!(f, "Disconnected from {}", host),
+            Disconnected {
+                host,
+                error: Some(error),
+            } => write!(f, "Disconnected from {} due to error: {}", host, error),
+
+            // The network module is sending the compiled [Action] to the host for execution.
+            RunningAction {
+                host,
+                manifest_source,
+                manifest_name,
+                task_source,
+                task_name,
+                action,
+            } => {
+                // TODO Replace this with a YAML serialization when possible.
+                write!(
+                    f,
+                    "Running action on {}:\n\
+                    \tManifest: {} ({})\n\
+                    \tTask: {} ({})\n\
+                    \tAction: {:?}",
+                    host,
+                    manifest_name,
+                    manifest_source.as_deref().unwrap_or("Unknown source"),
+                    task_name,
+                    task_source.as_deref().unwrap_or("Unknown source"),
+                    action,
+                )
+            }
+
+            // The specified [Action] is has finished running.
+            ActionResult {
+                host,
+                manifest_source,
+                manifest_name,
+                task_source,
+                task_name,
+                action,
+                result,
+            } => {
+                write!(
+                    f,
+                    "Action complete on {}:\n\
+                    \tManifest: {} ({})\n\
+                    \tTask: {} ({})\n\
+                    \tAction: {:?}\n\
+                    \tResult: {:?}",
+                    host,
+                    manifest_name,
+                    manifest_source.as_deref().unwrap_or("Unknown source"),
+                    task_name,
+                    task_source.as_deref().unwrap_or("Unknown source"),
+                    action,
+                    result,
+                )
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod report {
+        use super::*;
+
+        mod display {
+            use super::*;
+            use Report::*;
+
+            #[test]
+            fn connecting() {
+                assert_eq!(
+                    "Connecting to hostname",
+                    Connecting("hostname".to_string()).to_string(),
+                );
+            }
+
+            #[test]
+            fn connected() {
+                assert_eq!(
+                    "Connected to hostname",
+                    Connected("hostname".to_string()).to_string(),
+                );
+            }
+
+            #[test]
+            fn failed_to_connect() {
+                assert_eq!(
+                    "Couldn't connect to hostname: error",
+                    FailedToConnect {
+                        host: "hostname".to_string(),
+                        error: "error".to_string(),
+                    }
+                    .to_string(),
+                );
+            }
+
+            #[test]
+            fn disconnected() {
+                assert_eq!(
+                    "Disconnected from hostname",
+                    Disconnected {
+                        host: "hostname".to_string(),
+                        error: None,
+                    }
+                    .to_string(),
+                );
+
+                assert_eq!(
+                    "Disconnected from hostname due to error: error",
+                    Disconnected {
+                        host: "hostname".to_string(),
+                        error: Some("error".to_string()),
+                    }
+                    .to_string(),
+                );
+            }
+
+            #[test]
+            fn running_action() {
+                assert_eq!(
+                    "Running action on host:\n\
+                    \tManifest: mname (mani.fest)\n\
+                    \tTask: tname (ta.sk)\n\
+                    \tAction: Shell { commands: [\"pwd\"] }",
+                    RunningAction {
+                        host: "host".to_string(),
+                        manifest_source: Some("mani.fest".to_string()),
+                        manifest_name: "mname".to_string(),
+                        task_source: Some("ta.sk".to_string()),
+                        task_name: "tname".to_string(),
+                        action: Arc::new(Action::Shell {
+                            commands: vec!["pwd".to_string()],
+                        }),
+                    }
+                    .to_string(),
+                );
+
+                assert_eq!(
+                    "Running action on host:\n\
+                    \tManifest: mname (Unknown source)\n\
+                    \tTask: tname (Unknown source)\n\
+                    \tAction: Shell { commands: [\"pwd\"] }",
+                    RunningAction {
+                        host: "host".to_string(),
+                        manifest_source: None,
+                        manifest_name: "mname".to_string(),
+                        task_source: None,
+                        task_name: "tname".to_string(),
+                        action: Arc::new(Action::Shell {
+                            commands: vec!["pwd".to_string()],
+                        }),
+                    }
+                    .to_string(),
+                );
+            }
+
+            #[test]
+            fn action_result() {
+                assert_eq!(
+                    "Action complete on host:\n\
+                    \tManifest: mname (mani.fest)\n\
+                    \tTask: tname (ta.sk)\n\
+                    \tAction: Shell { commands: [\"pwd\"] }\n\
+                    \tResult: Ok(())",
+                    ActionResult {
+                        host: "host".to_string(),
+                        manifest_source: Some("mani.fest".to_string()),
+                        manifest_name: "mname".to_string(),
+                        task_source: Some("ta.sk".to_string()),
+                        task_name: "tname".to_string(),
+                        action: Arc::new(Action::Shell {
+                            commands: vec!["pwd".to_string()],
+                        }),
+                        result: Ok(()),
+                    }
+                    .to_string(),
+                );
+
+                assert_eq!(
+                    "Action complete on host:\n\
+                    \tManifest: mname (Unknown source)\n\
+                    \tTask: tname (Unknown source)\n\
+                    \tAction: Shell { commands: [\"pwd\"] }\n\
+                    \tResult: Ok(())",
+                    ActionResult {
+                        host: "host".to_string(),
+                        manifest_source: None,
+                        manifest_name: "mname".to_string(),
+                        task_source: None,
+                        task_name: "tname".to_string(),
+                        action: Arc::new(Action::Shell {
+                            commands: vec!["pwd".to_string()],
+                        }),
+                        result: Ok(()),
+                    }
+                    .to_string(),
+                );
+            }
+        }
+    }
 }
