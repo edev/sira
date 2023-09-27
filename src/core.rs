957a4fefc64ea26c11e9a4dd7fed179f07b69899
Dylan Laufenberg <edev455@gmail.com>
2023-09-27 10:38:46 -0700

    Add re-exports of Action, Manifest, Task, & Plan.
    Document them as inline.

--- src/core.rs
+++ src/core.rs
@@ -4,3 +4,15 @@ pub mod action;
 pub mod manifest;
 pub mod plan;
 pub mod task;
+
+#[doc(inline)]
+pub use action::Action;
+
+#[doc(inline)]
+pub use manifest::Manifest;
+
+#[doc(inline)]
+pub use plan::Plan;
+
+#[doc(inline)]
+pub use task::Task;
