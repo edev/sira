5ced4420e1e47ed9bb3a6491b9dccd37efd2a18a
Dylan Laufenberg <edev455@gmail.com>
2024-03-26 13:56:23 -0700

    Add ability to verify cryptographic signatures.

--- resources/etc/sira/keys/README.md
+++ resources/etc/sira/keys/README.md
@@ -2,6 +2,6 @@
 
 **action** and **manifest**: properly formed, sample SSH key pairs for use in testing.
 
-**unreadable**: a private SSH key with no permissions, used for generating error messages in `ssh-keygen`. No public key is needed, as this should never be allowed to sign a file. This file is automatically created and deleted by the relevant test and should not be committed to source control, especially since Git won't be able to read it!
+**unreadable** and **unreadable.pub**: fake SSH key files with no permissions, used for generating error messages in `ssh-keygen`. These files are automatically created and deleted by the relevant tests and should not be committed to source control, especially since Git won't be able to read them!
 
 **does_not_exist** and **doesnotexist**: reserved for tests that need to provide a key path that does not exist. No files with these names should be created.
--- src/crypto/test.rs
+++ src/crypto/test.rs
@@ -1,46 +1,49 @@
 use super::*;
-use std::fmt::Display;
 use std::fs::{self, File};
 use std::io::Read;
 use std::os::unix::fs::PermissionsExt;
 use std::path::PathBuf;
+use tempfile::NamedTempFile;
 
-// Returns the path to the /resources directory of the repository.
-fn resource_path() -> PathBuf {
-    Path::new(env!("CARGO_MANIFEST_DIR")).join("resources")
-}
-
-// Returns paths to a file and its signature inside of the /resources directory of the repository.
-// This function does not check whether either path exists; it simply computes the paths.
-fn file_signature_pair(file_path: impl Display + AsRef<Path>) -> (PathBuf, PathBuf) {
-    let base_path = resource_path();
-    let signature_path = format!("{file_path}.sig");
-    (base_path.join(file_path), base_path.join(signature_path))
+// Returns paths to a file inside of the /resources directory of the repository.
+// This function simply computes the path but does not check whether the path exists.
+fn resource_path(file_path: impl AsRef<Path>) -> PathBuf {
+    let mut path = Path::new(env!("CARGO_MANIFEST_DIR")).join("resources");
+    path.push(file_path);
+    path
 }
 
 // Signs a file in the file system (rather than in memory). Writes the resulting signature to the
 // file system. Overwrites any existing signature file.
-fn sign_file(file: impl AsRef<Path>, key: impl AsRef<Path>) -> anyhow::Result<SigningOutcome> {
+//
+// In the event of a successful signing, the second return value will contain the path to a
+// temporary file containing the signature. It is the caller's responsibility to delete this file.
+fn sign_file(
+    file: impl AsRef<Path>,
+    key: impl AsRef<Path>,
+) -> anyhow::Result<(SigningOutcome, Option<PathBuf>)> {
     let mut file_buffer = Vec::new();
     let _ = File::open(&file)?.read_to_end(&mut file_buffer)?;
     let signing_outcome = sign(&file_buffer, key)?;
+    let mut signature_file_path = None;
     if let SigningOutcome::Signed(ref signature) = signing_outcome {
-        let mut signature_file_name = file
-            .as_ref()
-            .file_name()
-            .expect("signed a file on disk but its path didn't end in a file name")
-            .to_owned();
-        signature_file_name.push(".sig");
-
-        let signature_path = file
-            .as_ref()
-            .parent()
-            .expect("signed a file on disk but couldn't retrieve its parent directory")
-            .join(signature_file_name);
-
-        fs::write(signature_path, signature)?;
+        let (mut file, path) = NamedTempFile::new()?.keep()?;
+        file.write_all(signature)?;
+        signature_file_path = Some(path);
     }
-    Ok(signing_outcome)
+    Ok((signing_outcome, signature_file_path))
+}
+
+// Reads a file from disk and calls `verify`.
+fn verify_file(
+    file: impl AsRef<Path>,
+    signature: impl AsRef<OsStr>,
+    allowed_signers: impl AsRef<Path>,
+    identity: impl AsRef<str>,
+) -> anyhow::Result<()> {
+    let mut file_buffer = Vec::new();
+    let _ = File::open(&file)?.read_to_end(&mut file_buffer)?;
+    verify(&file_buffer, signature, allowed_signers, identity)
 }
 
 mod sign {
@@ -51,21 +54,14 @@ mod sign {
 
     #[test]
     fn if_signing_succeeds_returns_signature() {
-        let (file_path, signature_path) = file_signature_pair("sample.manifest");
+        let file_path = resource_path("sample.manifest");
 
         let key = "manifest";
 
-        // Remove any previous signature, e.g. in case of a crashed test.
-        //
-        // We don't need to parse specific errors here, because if we can't remove the file, we
-        // will presumably encounter errors trying to write the file in sign_file, which handles
-        // and reports errors.
-        let _ = fs::remove_file(&signature_path);
-
         // Run the code under test using the sign_file wrapper.
-        match sign_file(&file_path, key) {
-            Ok(SigningOutcome::Signed(_)) => {}
-            x => panic!("expected Ok(SigningOutcome::Signed(_)) but received:\n{x:?}"),
+        let signature_path = match sign_file(&file_path, key) {
+            Ok((SigningOutcome::Signed(_), Some(signature_path))) => signature_path,
+            x => panic!("expected Ok((SigningOutcome::Signed(_), Some(path)) but received:\n{x:?}"),
         };
 
         // Now we verify that the signature is actually correct. We intentionally use a manual
@@ -73,12 +69,7 @@ mod sign {
         // crate::crypto::verify to prevent the possibility of codependent bugs in sign() and
         // verify() causing this test to erroneously pass.
 
-        let allowed_signers_path = {
-            let mut path = config::config_dir();
-            path.push(ALLOWED_SIGNERS_DIR);
-            path.push(key);
-            path
-        };
+        let allowed_signers_path = resource_dir(ALLOWED_SIGNERS_DIR).join(key);
 
         let signer_identity = {
             let mut allowed_signers = fs::read_to_string(&allowed_signers_path).unwrap();
@@ -113,7 +104,7 @@ mod sign {
 
     #[test]
     fn protects_from_directory_traversal_on_key() {
-        let (file, _) = file_signature_pair("sample.manifest");
+        let file = resource_path("sample.manifest");
         let file = {
             let mut file_buffer = Vec::new();
             let _ = File::open(file)
@@ -128,15 +119,18 @@ mod sign {
         // directory traversal.
         for key in ["/", "..", "/home"] {
             match sign(&file, key) {
-                Err(anyhow) if anyhow.to_string().contains("alphabetic characters") => {},
-                x => panic!("failed to detect directory traversal attack on key \"{key}\"; instead returned: {x:?}"),
+                Err(anyhow) if anyhow.to_string().contains("alphabetic characters") => {}
+                x => panic!(
+                    "failed to detect directory traversal attack on key \"{key}\"; instead \
+                    returned: {x:?}"
+                ),
             }
         }
     }
 
     #[test]
     fn rejects_empty_key_argument() {
-        let (file, _) = file_signature_pair("sample.manifest");
+        let file = resource_path("sample.manifest");
         let file = {
             let mut file_buffer = Vec::new();
             let _ = File::open(file)
@@ -153,7 +147,7 @@ mod sign {
 
     #[test]
     fn if_key_does_not_exist_returns_key_not_found() {
-        let (file, _) = file_signature_pair("sample.manifest");
+        let file = resource_path("sample.manifest");
         let file = {
             let mut file_buffer = Vec::new();
             let _ = File::open(file)
@@ -174,12 +168,7 @@ mod sign {
     fn returns_error_from_command() {
         let file = [];
 
-        let unreadable_path = {
-            let mut path = config::config_dir();
-            path.push(KEY_DIR);
-            path.push("unreadable");
-            path
-        };
+        let unreadable_path = resource_dir(KEY_DIR).join("unreadable");
 
         // Create an unreadable private key file. If it already exists (e.g. from a previously
         // terminated test pass), this will fail, and that's fine. After this line, though, all
@@ -200,6 +189,153 @@ mod sign {
 
         // Clean up the unreadable key file before we unwrap the result.
         fs::remove_file(unreadable_path).unwrap();
+
+        result.unwrap();
+    }
+}
+
+mod verify {
+    use super::*;
+
+    // I am not aware of any feasible way to test the various errors that can crop up when dealing
+    // with std::process::Command. Thus, these are not under test here.
+
+    #[test]
+    fn if_verification_succeeds_returns_ok() {
+        let file_path = resource_path("sample.manifest");
+
+        // The name of both the private key used to sign and the allowed_signers file used to
+        // verify the signature.
+        let key = "manifest";
+
+        // Sign the file using the sign_file wrapper.
+        //
+        // Note that there are no concerns about codependent bugs, here, because we have a test for
+        // sign() that mitigates that concern. Therefore, we can just use sign_file() here.
+        let signature_path = match sign_file(&file_path, key) {
+            Ok((SigningOutcome::Signed(_), Some(signature_path))) => signature_path,
+            x => panic!("expected Ok((SigningOutcome::Signed(_), Some(path)) but received:\n{x:?}"),
+        };
+
+        let allowed_signers_path = resource_dir(ALLOWED_SIGNERS_DIR).join(key);
+
+        // This will fail if the sample allowed_signers file has more than one entry and the entry
+        // that matches the signature is not the first one, but this is all we need for testing.
+        let identity = {
+            let mut allowed_signers = fs::read_to_string(allowed_signers_path).unwrap();
+            let identity_length = allowed_signers.find(' ').unwrap();
+            allowed_signers.truncate(identity_length);
+            allowed_signers
+        };
+
+        let result = verify_file(file_path, &signature_path, key, identity);
+
+        // Remove the signature file before asserting success.
+        fs::remove_file(&signature_path).unwrap();
+
+        result.unwrap();
+    }
+
+    #[test]
+    fn protects_from_directory_traversal_on_allowed_signers() {
+        let file = resource_path("sample.manifest");
+        let file = {
+            let mut file_buffer = Vec::new();
+            let _ = File::open(file)
+                .unwrap()
+                .read_to_end(&mut file_buffer)
+                .unwrap();
+            file_buffer
+        };
+
+        // These are not all possible types of directory traversals, but checking these will
+        // exercise the logic that requires keys to be alphabetic. This, in turn, prevents
+        // directory traversal.
+        for key in ["/", "..", "/home"] {
+            match verify(&file, "/dev/null", key, "sira") {
+                Err(anyhow) if anyhow.to_string().contains("alphabetic characters") => {}
+                x => panic!(
+                    "failed to detect directory traversal attack on allowed_signers \"{key}\"; \
+                    instead returned: {x:?}"
+                ),
+            }
+        }
+    }
+
+    #[test]
+    fn rejects_empty_allowed_signers_argument() {
+        let file = resource_path("sample.manifest");
+        let file = {
+            let mut file_buffer = Vec::new();
+            let _ = File::open(file)
+                .unwrap()
+                .read_to_end(&mut file_buffer)
+                .unwrap();
+            file_buffer
+        };
+        match verify(&file, "/dev/null", "", "sira") {
+            Err(anyhow) if anyhow.to_string().contains("should not be empty") => {}
+            x => panic!("failed to detect empty allowed_signers; instead returned: {x:?}"),
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "install the required allowed_signers file")]
+    fn allowed_signers_file_not_found() {
+        let file = resource_path("sample.manifest");
+        let file = {
+            let mut file_buffer = Vec::new();
+            let _ = File::open(file)
+                .unwrap()
+                .read_to_end(&mut file_buffer)
+                .unwrap();
+            file_buffer
+        };
+
+        verify(&file, "/dev/null", "doesnotexist", "sira").unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "allowed_signers/unreadable")]
+    fn returns_error_from_command() {
+        let file = resource_path("sample.manifest");
+        let file_buffer = fs::read_to_string(&file).unwrap().into_bytes();
+
+        // The name of the key to use for the portions of this test setup that require a
+        // functioning key.
+        let stand_in_key = "manifest";
+
+        // Create a real signature so OpenSSH can reach the point of failing to read
+        // allowed_signers/unreadable.
+        let signature_path = match sign_file(&file, stand_in_key) {
+            Ok((SigningOutcome::Signed(_), Some(signature_path))) => signature_path,
+            x => panic!("expected Ok((SigningOutcome::Signed(_), Some(path)) but received:\n{x:?}"),
+        };
+
+        let unreadable_path = resource_dir(ALLOWED_SIGNERS_DIR).join("unreadable");
+
+        // Create an unreadable allowed_signers file. If it already exists (e.g. from a previously
+        // terminated test pass), this will fail, and that's fine. After this line, though, all
+        // calls must succeed in order for the test to pass and are therefore unwrapped.
+        let _ = File::create(&unreadable_path);
+
+        // Remove all permissions on the allowed_signers file.
+        let mut permissions = fs::metadata(&unreadable_path).unwrap().permissions();
+        permissions.set_mode(0o0);
+        fs::set_permissions(&unreadable_path, permissions).unwrap();
+
+        // Hand ssh-keygen an allowed_signers file it can't read, so it returns an error message.
+        let result = verify(&file_buffer, &signature_path, "unreadable", "sira");
+
+        // Clean up the files we created before we unwrap the result from above. Delay unwrapping
+        // the results of these clean-up operations until they're all complete.
+        let delayed_results: Vec<std::io::Result<()>> = [unreadable_path, signature_path]
+            .iter()
+            .map(fs::remove_file)
+            .collect();
+        for result in delayed_results {
+            result.unwrap();
+        }
 
         result.unwrap();
     }
