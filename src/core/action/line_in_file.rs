009a90f6c992ab94da48f9083733d0efe77431d3
Dylan Laufenberg <edev455@gmail.com>
2024-04-17 06:54:59 -0700

    Fix typo in error message.

--- src/core/action/line_in_file.rs
+++ src/core/action/line_in_file.rs
@@ -34,7 +34,7 @@ pub fn line_in_file(action: &Action) -> io::Result<()> {
             after,
             indent,
         } => (path, line, pattern, after, indent),
-        _ => panic!("called _line_in_file with an Action that was not a LineInFile: {action:?}"),
+        _ => panic!("called line_in_file with an Action that was not a LineInFile: {action:?}"),
     };
 
     let mut file = fs::read_to_string(path)?;
